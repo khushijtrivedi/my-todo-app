892b07f500c8cfd1cd2f94a0cd861448fa30f11b
Khushi trivedi <khushi.trivedi.j@gmail.com>
2025-01-15 19:18:36 +0530

    fix: extension of css file for ddeployment 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/tailwind.CSS"; // Ensure Tailwind CSS is loaded
+import "./styles/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
